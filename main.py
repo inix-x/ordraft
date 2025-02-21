6b59039d79da986ee7e1f65e55b972adb6e5063a
Omar Gerardo <omargrard@gmail.com>
2025-02-21 15:31:37 +0800

    fixed: resolved  add item as queue item dynamic on dark theme config. chore: changes start value for pulsating animation

--- main.py
+++ main.py
@@ -1038,7 +1038,7 @@ class Window(FluentWindow):
             icon=CFIF.ACTIVE, 
             loop=False,
             id=data,
-            dark_theme=self.cfg.darkTheme
+            dark_theme=self.cfg.darkTheme.value
         )
         list_item = QListWidgetItem(self.prototype_interface.left_w)
         list_item.setSizeHint(queue_item.sizeHint())
