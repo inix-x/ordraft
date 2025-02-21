bab9adaee94213229bd6c3c38a20b15e733a838f
Omar Gerardo <omargrard@gmail.com>
2025-02-21 23:32:43 +0800

    fixed: resolved blocking ui when calling a thread to quit and wait

--- pkgs/main_vm.py
+++ pkgs/main_vm.py
@@ -3,7 +3,7 @@ import sys
 import traceback
 from pathlib import Path
 
-from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot, QThread, QEventLoop
+from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot, QThread, QEventLoop, QEventLoop
 
 if __name__ == "__main__" or "pkgs" not in sys.modules:
     test = sys.path.append(
